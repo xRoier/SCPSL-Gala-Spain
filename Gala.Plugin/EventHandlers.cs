89225e5df9e0ced7e23773f2b2a9c5ca18270100
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-06-13 19:39:19 +0200

    Comentario

--- Gala.Plugin/EventHandlers.cs
+++ Gala.Plugin/EventHandlers.cs
@@ -1,9 +1,10 @@
-﻿using MapEditorReborn.API.Features;
+using MapEditorReborn.API.Features;
 
 namespace Gala.Plugin
 {
     public class EventHandlers
     {
+        // MapEditorReborn: carga el mapa con el nombre especificado ubicado en EXILED/Configs/MapEditorReborn/Maps
         public void OnWaitingForPlayers()
         {
             MapUtils.LoadMap(MapUtils.GetMapByName("cambiar_esto_al_nombre_del_mapa"));
