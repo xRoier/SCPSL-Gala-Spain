b841f61f8f670e3852db6f7f93875f66a7f72eed
Roger <58123692+xRoier@users.noreply.github.com>
2022-06-17 17:05:11 +0200

    Espacios

--- Gala.Plugin/EventHandlers.cs
+++ Gala.Plugin/EventHandlers.cs
@@ -10,10 +10,12 @@ namespace Gala.Plugin
             // MapEditorReborn: carga el mapa con el nombre especificado ubicado en EXILED/Configs/MapEditorReborn/Maps
             MapUtils.LoadMap(MapUtils.GetMapByName("cambiar_esto_al_nombre_del_mapa"));
         }
+
         public void OnTransmitting(TransmittingEventArgs ev) // Transmitting = Chat de la V
         {
             ev.IsAllowed = false;
         }
+
         public void OnVoiceChatting(VoiceChattingEventArgs ev) // VoiceChatting = Chat de la Q
         {
             ev.IsAllowed = false;
