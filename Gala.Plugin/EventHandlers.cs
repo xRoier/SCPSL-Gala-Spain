dc28d425ea0256d274bb0cad03c22012c9f16c58
Sr.Gato <srrgatoo@gmail.com>
2022-06-17 16:05:46 +0200

    VC Desactivado en un principio

--- Gala.Plugin/EventHandlers.cs
+++ Gala.Plugin/EventHandlers.cs
@@ -1,4 +1,5 @@
 using MapEditorReborn.API.Features;
+using Exiled.Events.EventArgs;
 
 namespace Gala.Plugin
 {
@@ -8,6 +9,14 @@ namespace Gala.Plugin
         {
             // MapEditorReborn: carga el mapa con el nombre especificado ubicado en EXILED/Configs/MapEditorReborn/Maps
             MapUtils.LoadMap(MapUtils.GetMapByName("cambiar_esto_al_nombre_del_mapa"));
+        }
+        public void OnTransmitting(TransmittingEventArgs ev) // Transmitting = Chat de la V
+        {
+            ev.IsAllowed = false;
+        }
+        public void OnVoiceChatting(VoiceChattingEventArgs ev) // VoiceChatting = Chat de la Q
+        {
+            ev.IsAllowed = false;
         }
     }
 }
